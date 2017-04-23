2fbd68caf7545fc6b9b37048fc6f3e63dc64ebab
Jetsonhacks <jim@jetsonhacks.com>
2017-04-22 20:15:11 -0700

    NVIDIA certificate is self signed, don't check it

--- scripts/installKernelSources.sh
+++ scripts/installKernelSources.sh
@@ -3,7 +3,8 @@ apt-add-repository universe
 apt-get update
 apt-get install libqt4-dev pkg-config -y
 cd /usr/src
-wget http://developer.nvidia.com/embedded/dlc/l4t-sources-24-2-1 -O sources.tbz2
+# Currently the NVIDIA certificate is self signed, so don't check it
+wget --no-check-certificate http://developer.nvidia.com/embedded/dlc/l4t-sources-24-2-1 -O sources.tbz2
 tar -xvf sources.tbz2 source/kernel_src.tbz2
 tar -xvf source/kernel_src.tbz2
 cd kernel
