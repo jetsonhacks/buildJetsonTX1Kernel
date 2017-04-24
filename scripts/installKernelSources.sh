b5857f1eb0eb12ed59cd460b38bf71622cd76f83
Natalia Lyubova <natalia.lyubova@gmail.com>
2017-04-24 15:26:08 +0200

    adding a path to kernel and cleaning

--- scripts/installKernelSources.sh
+++ scripts/installKernelSources.sh
@@ -2,11 +2,20 @@
 apt-add-repository universe
 apt-get update
 apt-get install libqt4-dev pkg-config -y
-cd /usr/src
+if [ -z "$1" ]
+  then
+    DIR="/usr/src"
+else
+  DIR=$1
+fi
+
+cd $DIR
 # Currently the NVIDIA certificate is self signed, so don't check it
 wget --no-check-certificate http://developer.nvidia.com/embedded/dlc/l4t-sources-24-2-1 -O l4t-sources-24-2-1.tbz2
 tar -xvf l4t-sources-24-2-1.tbz2 sources/kernel_src.tbz2
+rm l4t-sources-24-2-1.tbz2
 tar -xvf sources/kernel_src.tbz2
+rm -rf sources
 cd kernel
 zcat /proc/config.gz > .config
 make xconfig
