4e25a9a6cfad25dd2adbe91525f47c3975321269
Natalia Lyubova <natalia.lyubova@gmail.com>
2017-04-24 12:25:05 +0200

    providing a path to the kernel

--- scripts/copyzImage.sh
+++ scripts/copyzImage.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-cd /usr/src/kernel
+if [ -z "$1" ]
+  then
+    DIR="/usr/src"
+else
+  DIR=$1
+fi
+
+cd $DIR/kernel
 sudo cp arch/arm64/boot/zImage /boot/zImage
 sudo cp arch/arm64/boot/Image /boot/Image
 
