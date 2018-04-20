c7bbf519d244b9f29ae9ccbe209f9a26a5854646
jetsonhacks <jim@jetsonhacks.com>
2018-04-19 20:53:47 -0700

    No GUI 28.2

---
+++ scripts/getKernelSourcesNoGUI.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt-add-repository universe
+apt-get update
+apt-get install pkg-config -y
+cd /usr/src
+wget http://developer.download.nvidia.com/embedded/L4T/r28_Release_v2.0/GA/BSP/tx1_sources.tbz2
+sudo tar -xvf tx1_sources.tbz2 public_release/kernel_src.tbz2
+tar -xvf public_release/kernel_src.tbz2
+# Space is tight get rid of the compressed kernel source
+rm -r public_release
+cd kernel/kernel-4.4
+zcat /proc/config.gz > .config
+
