e6ec90bf90c4e503303c9016c530f604ad1f9d4d
jetsonhacks <jim@jetsonhacks.com>
2018-04-20 14:50:36 -0700

    Add Reboot message

--- copyImage.sh
+++ copyImage.sh
@@ -5,6 +5,9 @@ if [ -f /usr/src/kernel/kernel-4.4/arch/arm64/boot/Image ] ; then
   echo "Copying Image to /boot diretory"
   sudo ./scripts/copyImage.sh
   echo "Image copied"
+  tput setaf 2
+  echo "Please reboot for changes to take effect"
+  tput sgr0
 else
   tput setaf 1
   echo "==== Image file does not exist ===="
